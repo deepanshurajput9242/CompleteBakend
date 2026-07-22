03576d9b432c2689010a49270825a71d5b62a87a
Deepanshu Rajput <deepanshu.rajput9242@gmail.com>
2026-07-23 02:08:58 +0530

    [changes in ReadMe File]

--- Day1/Code/server.js
+++ Day1/Code/server.js
@@ -1,3 +1,22 @@
 const express=require ('express');
-const app= express();
-app.listen(3000);
+const app= express();//server create kia h 
+
+
+app.get("/",(req,res)=>{//"/" api p request aati h toh aap response doge hello world
+ res.send("hello world");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("About Page");
+})
+app.listen(3001);// is line m server ko start kia h 
+// kya tumne server ko program kia?? response??
+
+
+
+// const express=require('express');
+// const app=express()
+// app.get("/",(req,res)=>{
+//     res.send("Hello World")
+// })
+// app.listen(3000); practice makes man perfect 
