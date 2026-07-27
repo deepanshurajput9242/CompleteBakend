3413bc7cd2d467dbabe691525180689df5d5dc80
Deepanshu Rajput <deepanshu.rajput9242@gmail.com>
2026-07-27 23:27:50 +0530

    [Changes pushed Day5 revised]

--- Day5/sRc/app.js
+++ Day5/sRc/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const multer=require('multer');
+const uploadFile=require("./services/storage.service")
 
 const app = express();
 
 app.use(express.json());
+
+const upload=multer({storage:multer.memoryStorage()})
+//multer ka use is baar aap file bhej rhe ho is bar text nhi bhej rhe toh file ko read krne k liye kuch tareeka hoga isisliye multer ko use kr rhe h 
+
+
+app.post('/create-post',upload.single("image") ,async(req,res)=>{
+    console.log(req.body);
+    console.log(req.file);
+
+    const result=await uploadFile(req.file.buffer)
+    console.log(result);
+    
+    
+    
+})
 
 module.exports = app;
--- Day5/sRc/db/db.js
+++ Day5/sRc/db/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 async function connectDB() {
     try {
         await mongoose.connect(
-            "mongodb+srv://yt:CwABTuEgqPIcEC2s@cluster0.vusgc5r.mongodb.net/project-1?retryWrites=true&w=majority&appName=Cluster0"
+            // "mongodb+srv://yt:CwABTuEgqPIcEC2s@cluster0.vusgc5r.mongodb.net/project-1?retryWrites=true&w=majority&appName=Cluster0"
+           "mongodb://yt:CwABTuEgqPIcEC2s@ac-iqm07jr-shard-00-00.vusgc5r.mongodb.net:27017,ac-iqm07jr-shard-00-01.vusgc5r.mongodb.net:27017,ac-iqm07jr-shard-00-02.vusgc5r.mongodb.net:27017/?ssl=true&replicaSet=atlas-a4h7l4-shard-0&authSource=admin&appName=Cluster0/project-0"
         );
 
         console.log("✅ Connected to MongoDB");
